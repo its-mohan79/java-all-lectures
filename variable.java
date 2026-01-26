0fd72a84f7e2c47178b626bbd6b501b5ab4cde54
its-mohan79 <mohanroy8989@gmail.com>
2026-01-26 13:47:09 +0530

    lesson of variable is complete

---
+++ variable.java
@@ -0,0 +1,25 @@
+public class variable{
+  public static void main(String[] args) { 
+    // variables declration
+    int a;
+    String name;
+    
+// variable initialization
+
+    a = 10;
+    name = "java";
+    System.out.println(a);
+    System.out.println(name);
+
+
+
+
+
+
+  }
+
+
+
+
+
+}
