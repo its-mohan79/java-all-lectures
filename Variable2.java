2f8c195994d9a4ac5a1d5193ec9cd04648094cad
its-mohan79 <mohanroy8989@gmail.com>
2026-01-27 22:11:43 +0530

    complete the lesson 3

--- Variable2.java
+++ Variable2.java
@@ -1,14 +1,26 @@
+import javax.print.DocFlavor.STRING;
+
 public class Variable2 {
   public static void main(String[] args){
 
 
-
-
+// variables declration
+    int a;
+    String name;
     
+// variable initialization
+
+    a = 10;
+    name = "java";
+    int age  = 12;
+    
+    System.out.println("Hello, " + name + ". You are " + age + " years old");
+
+
+
+
   }
     
-
-
 
 
 
