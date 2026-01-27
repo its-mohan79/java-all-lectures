737e1bb89feb3c9a40975252df4ea1be7fb6de0f
its-mohan79 <mohanroy8989@gmail.com>
2026-01-27 22:38:42 +0530

    solve the variable quiz

---
+++ quizvariable.java
@@ -0,0 +1,20 @@
+public class quizvariable {
+  public static void main(String[] args) {
+
+    String name = "mohan";
+    int age = 25;
+    String language = "java";
+
+    System.out.printf(
+      "Hello, %s. You are %d years old. You love %s.%n",
+      name, age, language
+    );
+
+
+
+
+    System.out.println(
+      "Hello, " + name + ". You are " + age + " years old. You love " + language + "."
+    );
+  }
+}
