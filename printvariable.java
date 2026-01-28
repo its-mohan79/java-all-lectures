5b18f9370feae1d52ecf5dd5fa4483f8ae72b0d3
its-mohan79 <mohanroy8989@gmail.com>
2026-01-28 13:22:45 +0530

    print variable is completed

---
+++ printvariable.java
@@ -0,0 +1,30 @@
+public class printvariable {
+  public static void main(String[] args) {
+
+
+String firstName = "Mohan ";
+String lastName = "Kumar";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+
+
+
+int x = 5;
+int y = 6;
+System.out.println(x + y); // Print the value of x + y
+
+
+
+int x1 = 5;
+int y2 = 6;
+
+System.out.println("The sum is " + x1 + y2);   // Prints: The sum is 56
+System.out.println("The sum is " + (1 + y2)); // Prints: The sum is 11
+
+  }  
+
+
+
+
+
+}
