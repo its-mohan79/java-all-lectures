e3c4574416dbb6a19eabe28f6b813ad3ecf70b7c
its-mohan79 <mohanroy8989@gmail.com>
2026-01-28 13:35:57 +0530

    compelete the indetifier variable chapter

--- identifiervariable.java
+++ identifiervariable.java
@@ -3,16 +3,16 @@ public class identifiervariable {
 
 
 // Good
-int minutesPerHour = 60;
+// int minutesPerHour = 60;
 
-// OK, but not so easy to understand what m actually is
-int m = 60;
+// // OK, but not so easy to understand what m actually is
+// int m = 60;
 
 
 // Invalid identifiers:
-int 2ndNumber = 5;  // Cannot start with a digit
-int my var = 10;    // Cannot contain spaces
-int int = 20;       // Cannot use reserved keywords
+// int 2ndNumber = 5;  // Cannot start with a digit
+// int my var = 10;    // Cannot contain spaces
+// int int = 20;       // Cannot use reserved keywords
 
    } 
 
