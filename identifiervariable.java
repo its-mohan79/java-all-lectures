3dccfce0da2eebc13ec71c0b8e85dfc1659d8b5a
its-mohan79 <mohanroy8989@gmail.com>
2026-01-28 13:34:26 +0530

    complete the indentifier variable chapter

---
+++ identifiervariable.java
@@ -0,0 +1,22 @@
+public class identifiervariable {
+   public static void main(String[] args){
+
+
+// Good
+int minutesPerHour = 60;
+
+// OK, but not so easy to understand what m actually is
+int m = 60;
+
+
+// Invalid identifiers:
+int 2ndNumber = 5;  // Cannot start with a digit
+int my var = 10;    // Cannot contain spaces
+int int = 20;       // Cannot use reserved keywords
+
+   } 
+
+
+
+
+}
