7f680b5b4c08242f1b168542110eec81a49fa7cd
its-mohan79 <mohanroy8989@gmail.com>
2026-01-28 13:30:55 +0530

    complete the multivariable chapter

---
+++ multiplevariable.java
@@ -0,0 +1,32 @@
+public class multiplevariable {
+   public static void main(String[] args) {
+
+int x = 5;
+int y = 6;
+int z = 50;
+System.out.println(x + y + z); // 61
+
+
+int x1 = 5, y1 = 6, z1 = 50;
+System.out.println(x1 + y1 + z1); // 61
+
+
+
+int x2, y2, z2;
+x2 = y2 = z2 = 50;
+System.out.println(x2 + y2 + z2); // 150
+
+
+
+
+
+
+
+   }
+
+
+
+
+
+    
+}
