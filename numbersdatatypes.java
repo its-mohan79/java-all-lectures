9d14de166d668b5b2938ddf6a8e085878bbf77ff
its-mohan79 <mohanroy8989@gmail.com>
2026-01-29 13:55:18 +0530

    complete this chapter

---
+++ numbersdatatypes.java
@@ -0,0 +1,32 @@
+public class numbersdatatypes{
+    public static void main(String[] args) {
+
+
+byte Mynum = 120;
+System.out.println(Mynum);
+
+
+short myNum = 5000;
+System.out.println("My number is: "+myNum);
+
+
+
+int mNum = 100000;
+System.out.println(mNum);
+
+long yNum = 15000000000L;
+System.out.println(yNum);
+
+
+
+
+
+float kyNum = 5.75f;
+System.out.println(kyNum);
+
+
+double lyNum = 19.99d;
+System.out.println(lyNum);
+    }
+    
+}
