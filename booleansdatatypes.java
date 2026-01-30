6ff9da97b4e021dc7bae128548a9a51933309fde
its-mohan79 <mohanroy8989@gmail.com>
2026-01-30 19:06:03 +0530

    complete the boolean chapter

---
+++ booleansdatatypes.java
@@ -0,0 +1,47 @@
+public class booleansdatatypes {
+    public static void main(String[] args){
+
+
+
+        boolean isServerRunning = true;
+
+        System.out.println(isServerRunning);
+    
+
+
+
+
+
+        int age = 19;
+        boolean canVote = age >= 18;
+
+        System.out.println(canVote);
+
+
+
+         boolean hasLicense = true;
+        boolean hasHelmet = false;
+
+        boolean allowedToRide = hasLicense && hasHelmet;
+
+        System.out.println(allowedToRide);
+    
+
+
+        
+        boolean hasID = false;
+        boolean hasGatePass = true;
+
+        boolean entryAllowed = hasID || hasGatePass;
+
+        System.out.println(entryAllowed);
+
+
+
+
+
+
+
+
+    }
+}
