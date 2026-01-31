954f20a3ad0a4d02aeb1be95d6a9ae6c329b8e9c
its-mohan79 <mohanroy8989@gmail.com>
2026-01-31 18:06:06 +0530

    complete this lecture

---
+++ varkeyword.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class varkeyword {
+    public static void main(String[] args){
+
+var x = 5;  // x is an int
+System.out.println(x);
+
+
+
+//for example
+
+// var myNum = 5;         // int
+// var myDouble = 9.98;   // double
+// var myChar = 'D';      // char
+// var myBoolean = true;  // boolean
+// var myString = "Hello"; // String
+
+
+// var x; // Error
+// var x = 5;  // OK
+
+
+// var x = 5;  // x is now an int
+// x = 10;     // OK - still an int
+// x = 9.99;   // Error - can't assign a double to an int
+
+
+
+   
+
+
+}    
+}
