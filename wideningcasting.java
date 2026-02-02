85fc141c01f70fe18ddf3b2af9b574ffe7c6486e
its-mohan79 <mohanroy8989@gmail.com>
2026-02-02 23:24:56 +0530

    learn widening casting in java

---
+++ wideningcasting.java
@@ -0,0 +1,16 @@
+public class wideningcasting {
+    public static void main(String[] args){
+
+int myint = 9;
+double mydouble = myint;
+System.out.println(myint);
+System.out.println(mydouble);
+
+
+
+
+
+
+    }
+    
+}
