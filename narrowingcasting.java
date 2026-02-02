b7f516230e8874c12862aea2424e463e84255774
its-mohan79 <mohanroy8989@gmail.com>
2026-02-02 23:38:31 +0530

    learn narrowing casting in java

---
+++ narrowingcasting.java
@@ -0,0 +1,31 @@
+public class narrowingcasting {
+    public static void main(String[] args){
+
+
+double mydouble = 9.78d;
+int myint = (int) mydouble;
+System.out.println(mydouble);
+System.out.println(myint);
+
+
+
+
+ int score = 65;
+        char grade = (char) score;   // int → char (narrowing)
+
+        System.out.println(grade);
+
+
+
+
+           double salary = 45890.75;
+        int finalSalary = (int) salary;   // narrowing
+
+        System.out.println(finalSalary);
+
+
+
+
+    }
+    
+}
