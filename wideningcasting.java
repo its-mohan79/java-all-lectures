b834ee76812bea30ea1a9ab5d0ca15015d122664
its-mohan79 <mohanroy8989@gmail.com>
2026-02-02 23:31:20 +0530

    learn widening casting in java

--- wideningcasting.java
+++ wideningcasting.java
@@ -9,7 +9,37 @@ System.out.println(mydouble);
 
 
 
+        int employees = 120;
+        double totalEmployees = employees;   // widening casting
 
+        System.out.println("Employees: " + employees);
+        System.out.println("Total Employees (double): " + totalEmployees);
+
+
+
+          int hoursWorked = 45;          // from attendance system
+        double hourlyRate = 250.75;    // finance system
+
+        double salary = hoursWorked * hourlyRate; // int → double (widening)
+
+        System.out.println("Salary: " + salary);
+
+
+
+
+
+
+        char grade = 'A';
+        int score = grade;   // char → int (widening)
+
+        System.out.println(score);
+    
+
+
+
+
+
+    
 
     }
     
