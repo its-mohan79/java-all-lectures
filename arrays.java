1811d295f05d7e53386e00d371573308dcf4ae43
its-mohan79 <mohanroy8989@gmail.com>
2026-02-02 22:54:46 +0530

    today learn array

---
+++ arrays.java
@@ -0,0 +1,16 @@
+public class arrays {
+    public static void main(String[] args){
+
+int [] marks = {1,2,3,4};
+
+System.out.println(marks[0]);
+System.out.println(marks[1]);
+System.out.println(marks[3]);
+
+
+
+
+
+    }
+    
+}
