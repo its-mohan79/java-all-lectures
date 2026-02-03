e02899d59316a831d95e9a0b1f9558503bc6de53
its-mohan79 <mohanroy8989@gmail.com>
2026-02-03 23:43:36 +0530

    today i learn arithmetic operator in java

--- arithmeticoperator.java
+++ arithmeticoperator.java
@@ -19,6 +19,17 @@ System.out.println(z); // 5
 
 
 
+
+
+
+
+ int a = 10;
+    int b = 3;
+    System.out.println(a / b);   // Integer division, result is 3
+
+    double c = 10.0d;
+    double d = 3.0d;
+    System.out.println(c / d);   // Decimal division, result is 3.333...
     }
     
 }
