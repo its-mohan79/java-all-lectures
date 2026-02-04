605353b5d18b91dbb8c68d4aedef3fee8e91f813
its-mohan79 <mohanroy8989@gmail.com>
2026-02-04 22:46:00 +0530

    today i learn assignment

---
+++ assignementvalue.java
@@ -0,0 +1,66 @@
+public class assignementvalue {
+    public static void main(String[] args){
+
+         int x = 5;
+    System.out.println(x);
+
+
+    int x2 = 5;
+    x2 += 3;
+    System.out.println(x2);
+
+
+
+    int x3 = 5;
+    x3 -= 3;
+    System.out.println(x3);
+
+
+    int x4 = 5;
+    x4 *= 3;
+    System.out.println(x4);
+
+
+    double x5 = 5;
+    x5 /= 3;
+    System.out.println(x5);
+
+
+     int x6 = 5;
+    x6 %= 3;
+    System.out.println(x6);
+
+
+    int x7 = 5;
+    x7 &= 3;
+    System.out.println(x7);
+
+
+
+     int x8 = 5;
+    x8 |= 3;
+    System.out.println(x8);
+
+
+     int x9 = 5;
+    x9 ^= 3;
+    System.out.println(x9);
+
+
+
+     int x10 = 5;
+    x10 >>= 3;
+    System.out.println(x10);
+
+
+        int x11 = 5;
+    x11 <<= 3;
+    System.out.println(x11);
+
+
+
+
+
+
+    }
+}
