cab5f9ce09b05144dba81fab97b3fa78ba9c8084
its-mohan79 <mohanroy8989@gmail.com>
2026-02-05 22:51:05 +0530

    today i learn about camparison operator

---
+++ comparisonoperator.java
@@ -0,0 +1,20 @@
+public class comparisonoperator {
+    public static void main(String[] args){
+
+int x = 5;
+    int y = 3;
+    System.out.println(x == y); // returns false because 5 is not equal to 3
+
+
+    boolean isLoggedIn = true;
+    boolean isAdmin = false;
+
+    System.out.println("Regular user: " + (isLoggedIn && !isAdmin));
+    System.out.println("Has access: " + (isLoggedIn || isAdmin));
+    System.out.println("Not logged in: " + (!isLoggedIn));
+
+
+
+    }
+    
+}
