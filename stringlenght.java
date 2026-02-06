026155553eb0bab19cc5abcab61d2046ce1dce45
its-mohan79 <mohanroy8989@gmail.com>
2026-02-06 23:28:16 +0530

    today i learn string lenght in java

---
+++ stringlenght.java
@@ -0,0 +1,11 @@
+public class stringlenght {
+    public static void main(String[] args){
+
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
+  }
+
+
+
+
+    }
