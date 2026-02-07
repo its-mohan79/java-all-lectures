c94b34c9631551668bc6a68006d8d52da273a1cc
its-mohan79 <mohanroy8989@gmail.com>
2026-02-08 00:02:24 +0530

    today i learn strings method

--- stringlenght.java
+++ stringlenght.java
@@ -17,6 +17,18 @@ public class stringlenght {
         String txt5 = "Hello";
     System.out.println(txt5.charAt(0));  // H
     System.out.println(txt5.charAt(4));  // o
+
+String value1 = "mohan";
+String value2 = "ankit";
+
+String value3 = "sohan";
+String value4 = "sohan";
+
+System.out.println(value1.equals(value2));
+System.out.println(value3.equals(value4));
+
+
+
   }
 
 
