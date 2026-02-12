c185f8f3b7c5b829ea9f5c3bd184fce9988e0ae6
its-mohan79 <mohanroy8989@gmail.com>
2026-02-12 22:45:17 +0530

    today i learn java contonation

---
+++ contonation.java
@@ -0,0 +1,14 @@
+public class contonation {
+    public static void main(String[] args){
+
+
+     String firstName = "mohan";
+    String lastName = "kumar";
+    System.out.println(firstName + " " + lastName);
+
+
+
+    }
+
+    
+}
