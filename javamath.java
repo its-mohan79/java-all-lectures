aed396de54b6d36fff84ed836883f9a739bfe1ea
its-mohan79 <mohanroy8989@gmail.com>
2026-02-13 22:06:30 +0530

    today i learn find max value in term of x and y

---
+++ javamath.java
@@ -0,0 +1,15 @@
+public class javamath {
+    public static void main(String[] args){
+
+//   The Math.max(x,y) method can be used to find the highest value of x and y:
+
+    System.out.println(Math.max(5, 10));  
+
+
+
+
+
+
+    }
+    
+}
