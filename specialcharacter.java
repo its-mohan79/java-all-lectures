bf3a2a4be072cb3eb14abed639a0bb543327629a
its-mohan79 <mohanroy8989@gmail.com>
2026-02-13 21:59:42 +0530

    today i learn specialcharacter in java

---
+++ specialcharacter.java
@@ -0,0 +1,46 @@
+public class specialcharacter {
+    public static void main(String[] aegs){
+
+//The sequence \"  inserts a double quote in a string:
+        String txt = "We are the so-called \"Vikings\" from the north.";
+          System.out.println(txt);
+
+
+
+          //The sequence \'  inserts a single quote in a string:
+
+
+ String txt1 = "It\'s alright.";
+    System.out.println(txt1);
+
+
+//The sequence \\  inserts a single backslash in a string:
+
+
+    String txt2= "The character \\ is called backslash.";
+    System.out.println(txt2);
+
+
+    //    /n is use new line
+
+     String txt4 = "Hello\nWorld!";
+    System.out.println(txt4);
+
+
+
+    //    /t is used for tab space
+
+     String txt5 = "Hello\tWorld!";
+    System.out.println(txt5);
+
+
+    //   /b is used for the backspace
+
+    String txt6 = "Hel\blo World!";
+    System.out.println(txt6);
+
+
+    }
+
+    
+}
