d5efa7bd196d4cb15d72a4ae16fb91c6c5553444
its-mohan79 <mohanroy8989@gmail.com>
2026-02-17 23:11:23 +0530

    today i learn if condition in java

--- ifjava.java
+++ ifjava.java
@@ -1,13 +1,19 @@
 public class ifjava {
     public static void main(String[] args){
 
+         boolean isRaining = true;
+
+    if (isRaining) {
+      System.out.println("Bring an umbrella!");
+    }
 
 
 
 
 
 
-        
+
+
     }
     
 }
