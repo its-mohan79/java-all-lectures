1b07bced628429d6eb23eee24f89963a502afd4f
its-mohan79 <mohanroy8989@gmail.com>
2026-04-07 22:55:28 +0530

    today i learn previouse topic in java

--- contonation.java
+++ contonation.java
@@ -9,7 +9,7 @@ public class contonation {
 
 
      String name = "mohan";
-    int age = 41;
+    int age = 31;
     System.out.println("My name is " + name + " and I am " + age + " years old.");
 
 
