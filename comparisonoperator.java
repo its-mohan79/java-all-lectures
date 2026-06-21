e5b78b0bcb57978826c516c7bc43bf18ed084b2a
its-mohan79 <mohanroy8989@gmail.com>
2026-06-22 00:07:53 +0530

    today

--- comparisonoperator.java
+++ comparisonoperator.java
@@ -1,7 +1,7 @@
 public class comparisonoperator {
     public static void main(String[] args){
 
-int x = 5;
+int x = 55;
     int y = 5;
     System.out.println(x == y); // returns false because 5 is not equal to 3
 
@@ -15,7 +15,6 @@ int x = 5;
     System.out.println("Not logged in: " + (!isLoggedIn));
 
 
-6464
     }
     
 }
