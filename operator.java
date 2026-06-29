20ef124af81cd1570351e0d245c5224ab5b70890
its-mohan79 <mohanroy8989@gmail.com>
2026-06-29 23:55:58 +0530

    today i learn  preeviouse topic in java

--- operator.java
+++ operator.java
@@ -4,7 +4,7 @@ public class operator {
 int x = 100 + 50;
 System.out.println(x);
 
- int sum1 = 10000 + 50;
+ int sum1 = 1000 + 50;
     int sum2 = sum1 + 250;
     int sum3 = sum2 + sum2;
     System.out.println(sum1);
